5f5ad185e1defa8015076e18477bea27d7a8655e
zhavei <zhavei.vei@gmail.com>
2023-11-21 16:49:59 +0700

    finish

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -91,3 +91,206 @@ for column in datetime_columns:
     all_df[column] = pd.to_datetime(all_df[column])
     
 ## membuat komponen filter
+
+min_date = all_df["order_date"].min()
+max_date = all_df["order_date"].max()
+
+with st.sidebar:
+    # menambah logo
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    #Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label="Rentang Waktu", min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+    
+'''Kode di atas akan menghasilkan start_date dan end_date yang selanjutnya akan digunakan untuk memfilter DataFrame. 
+Berikut merupakan tampilan sidebar yang akan dihasilkan dari kode tersebut. 
+Nah, start_date dan end_date di atas akan digunakan untuk memfilter all_df. Data yang telah difilter ini selanjutnya 
+akan disimpan dalam main_df. Proses ini dijalankan menggunakan kode berikut.'''
+
+main_df = all_df[(all_df["order_date"] >= str(start_date)) & 
+                (all_df["order_date"] <= str(end_date))]
+
+'''DataFrame yang telah difilter (main_df) inilah yang digunakan untuk menghasilkan berbagai DataFrame yang dibutuhkan 
+untuk membuat visualisasi data. Proses ini tentunya dilakukan dengan memanggil helper function yang telah kita buat sebelumnya.'''
+
+daily_orders_df = create_daily_orders_df(main_df)
+sum_order_items_df = create_sum_order_items_df(main_df)
+bygender_df = create_bygender_df(main_df)
+byage_df = create_byage_df(main_df)
+bystate_df = create_bystate_df(main_df)
+rfm_df = create_rfm_df(main_df)
+
+#Melengkapi Dashboard dengan Berbagai Visualisasi Data
+# membuat daily orders
+
+st.header('Dicoding Collection Dashboard :sparkles:')
+
+st.subheader('Daily Orders')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    total_orders = daily_orders_df.order_count.sum()
+    st.metric("total orders", value=total_orders)
+    
+with col2:
+    total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD", locale='es_CO') 
+    st.metric("Total Revenue", value=total_revenue)
+    
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_orders_df["order_date"],
+    daily_orders_df["order_count"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+
+st.pyplot(fig)
+
+
+#kita juga perlu menyertakan informasi tentang performa penjualan dari setiap produk. 
+#Pada proyek ini, kita akan menampilkan 5 produk paling laris dan paling sedikit terjual melalui sebuah visualisasi data.
+
+st.subheader("Best & Worst Performing Product")
+ 
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+ 
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+ 
+sns.barplot(x="quantity_x", y="product_name", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+ 
+sns.barplot(x="quantity_x", y="product_name", data=sum_order_items_df.sort_values(by="quantity_x", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+ 
+st.pyplot(fig)
+
+'''Informasi berikutnya yang ingin kita tampilkan pada dashboard tersebut ialah terkait demografi pelanggan yang kita miliki. 
+Pada materi Latihan Membuat Visualisasi Data, kita telah membuat tiga buah visualisasi data untuk menjelaskan hal tersebut. Nah, 
+kita akan memasukkan ketiganya ke dalam dashboard dengan kode seperti berikut.'''
+
+st.subheader("Customer Demographics")
+ 
+col1, col2 = st.columns(2)
+ 
+with col1:
+    fig, ax = plt.subplots(figsize=(20, 10))
+    
+    colors = ["#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+ 
+    sns.barplot(
+        y="customer_count", 
+        x="gender",
+        data=bygender_df.sort_values(by="customer_count", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+    ax.set_title("Number of Customer by Gender", loc="center", fontsize=50)
+    ax.set_ylabel(None)
+    ax.set_xlabel(None)
+    ax.tick_params(axis='x', labelsize=35)
+    ax.tick_params(axis='y', labelsize=30)
+    st.pyplot(fig)
+ 
+with col2:
+    fig, ax = plt.subplots(figsize=(20, 10))
+    
+    colors = ["#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+ 
+    sns.barplot(
+        y="customer_count", 
+        x="age_group",
+        data=byage_df.sort_values(by="age_group", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+    ax.set_title("Number of Customer by Age", loc="center", fontsize=50)
+    ax.set_ylabel(None)
+    ax.set_xlabel(None)
+    ax.tick_params(axis='x', labelsize=35)
+    ax.tick_params(axis='y', labelsize=30)
+    st.pyplot(fig)
+ 
+fig, ax = plt.subplots(figsize=(20, 10))
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    x="customer_count", 
+    y="state",
+    data=bystate_df.sort_values(by="customer_count", ascending=False),
+    palette=colors,
+    ax=ax
+)
+ax.set_title("Number of Customer by States", loc="center", fontsize=30)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
+
+'''Informasi terakhir yang harus kita tampilkan ialah terkait parameter RFM 
+(Recency, Frequency, & Monetary). Nah, pada proyek ini, kita akan menampilkan 
+nilai average atau rata-rata dari ketiga parameter tersebut menggunakan widget metric(). 
+Selain itu kita juga akan menampilkan hasil visualisasi data dari latihan sebelumnya. '''
+
+st.subheader("Best Customer Based on RFM Parameters")
+ 
+col1, col2, col3 = st.columns(3)
+ 
+with col1:
+    avg_recency = round(rfm_df.recency.mean(), 1)
+    st.metric("Average Recency (days)", value=avg_recency)
+ 
+with col2:
+    avg_frequency = round(rfm_df.frequency.mean(), 2)
+    st.metric("Average Frequency", value=avg_frequency)
+ 
+with col3:
+    avg_frequency = format_currency(rfm_df.monetary.mean(), "AUD", locale='es_CO') 
+    st.metric("Average Monetary", value=avg_frequency)
+ 
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
+colors = ["#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9", "#90CAF9"]
+ 
+sns.barplot(y="recency", x="customer_id", data=rfm_df.sort_values(by="recency", ascending=True).head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("customer_id", fontsize=30)
+ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
+ax[0].tick_params(axis='y', labelsize=30)
+ax[0].tick_params(axis='x', labelsize=35)
+ 
+sns.barplot(y="frequency", x="customer_id", data=rfm_df.sort_values(by="frequency", ascending=False).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("customer_id", fontsize=30)
+ax[1].set_title("By Frequency", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=30)
+ax[1].tick_params(axis='x', labelsize=35)
+ 
+sns.barplot(y="monetary", x="customer_id", data=rfm_df.sort_values(by="monetary", ascending=False).head(5), palette=colors, ax=ax[2])
+ax[2].set_ylabel(None)
+ax[2].set_xlabel("customer_id", fontsize=30)
+ax[2].set_title("By Monetary", loc="center", fontsize=50)
+ax[2].tick_params(axis='y', labelsize=30)
+ax[2].tick_params(axis='x', labelsize=35)
+ 
+st.pyplot(fig)
+ 
+st.caption('Copyright (c) Dicoding 2023')
